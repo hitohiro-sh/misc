1c64a625224dd754e72126a93a83dbc271167e6f
Hitohiro Shiozaki <h.shiozaki.it.just@gmail.com>
2022-04-12 09:59:53 +0900

    workspace

---
+++ functional/work/go.mod
@@ -0,0 +1,7 @@
+module work
+
+go 1.18
+
+replace functional => ../
+
+require functional v0.0.0-00010101000000-000000000000
---
+++ functional/work/work.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	f "functional"
+)
+
+func main() {
+	fmt.Println("Hey!")
+
+	var vs = f.Map([]int{1, 2, 3}, func(v int) int {
+		return v * v
+	})
+
+	fmt.Println(vs)
+}
